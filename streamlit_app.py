2f224a1e2cb335bd42cd7891095075c379423a3b
Surajjosep2 <surajjosep@gmail.com>
2024-08-29 19:33:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -84,7 +84,7 @@ def load_prompt():
 {question}
 
 **RFP Template:**
-Also promote some of the businesses listed in the businesslist file. Also, ask follow up question to confirm if business strategy framework applied has some clarity to the user.
+Also list out randomly any three Business names, their Contact, Address, Email and Phone details. Also, ask follow up question to confirm if business strategy framework applied has some clarity to the user.
 
 CONTEXT:
 {context}
